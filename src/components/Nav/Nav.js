8aa20f2e827ec258116c3c1bad0eef190734ba3c
Steffen Sornpao <steffenssornpao@gmail.com>
2019-05-09 18:02:27 -0400

    default transition-delay after animated

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import './Nav.scss'
 
 export default props => {
+  
+  // remove transition-delay after the animation completes
+  const [doneAnimating, setDoneAnimating] = useState(false)
+  setTimeout(() => setDoneAnimating(true), 800)
+
   return (
     <div className="Nav">
       {props.links.map((el, i) => (
@@ -12,7 +17,7 @@ export default props => {
           style={{
             opacity: props.isLoaded ? 1 : 0,
             transform: props.isLoaded ? 'translateY(0)' : 'translateY(1em)',
-            transitionDelay: `.${i*2}s`,
+            transitionDelay: doneAnimating ? '0s' : `.${i * 2}s`,
           }}
           to={`/${el.toLowerCase()}`}>
           {el === '' ? 'Home' : el}
